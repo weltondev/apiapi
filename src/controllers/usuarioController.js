8dd6a27ca3c90c2ff50e9ec562f80250fb4c2491
weltonDev <welton.developer95@gmail.com>
2024-02-28 11:30:16 -0300

    teste

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -30,7 +30,7 @@ const usuarioController = {
         return res.status(400).json(`Senhas não conferem!`);
       }
 
-      // Verifica se o usuário já existe
+      // Verifica se o usuário já exist
       if(userExist) {
         return res.status(400).json(`Usuário já cadastrado!`);
       }
