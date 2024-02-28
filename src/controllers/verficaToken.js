0f66426f72dba26621d7480fd14f4f7f0e13d997
weltonDev <welton.developer95@gmail.com>
2024-02-28 15:44:50 -0300

    auth

--- src/controllers/verficaToken.js
+++ src/controllers/verficaToken.js
@@ -4,7 +4,7 @@ const secret = 'univesp';
 function verificaToken(req, res, next){
   const token = req.headers['x-access-token'];
   jwt.verify(token, secret, (error, decoded) => {
-    if(error) return res.status(401).json(error);
+    if(error) return res.status(401).json({ msg: `Não Autorizado! ${error}` });
 
     console.log("passou");
     next();
