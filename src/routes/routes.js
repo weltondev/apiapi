fd85b4be357db0f8927f82f368a528e3985eda35
weltonDev <welton.developer95@gmail.com>
2024-02-29 19:35:30 -0300

    autorizado

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -6,7 +6,7 @@ const routes = express.Router();
 //Controllers
 routes.post('/login', usuarioController.login); // Login
 
-routes.get('/users', verificaToken, usuarioController.listar); // Listar usuários
+routes.get('/users', usuarioController.listar); // Listar usuários
 routes.post('/users', usuarioController.criar); // Criar usuários
 routes.put('/users/:id', usuarioController.atualizar); // Atualizar usuários
 routes.delete('/users/:id', usuarioController.remover); // remover usuários
